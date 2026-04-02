f093d3dbc8d96281d842ad8f4e289637dcc75f45
polluste888 <bert-robert.polluste@voco.ee>
2026-04-02 14:14:00 +0300

    ül8

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
+import React, {useReducer} from 'react';
 import Header from './components/Header.js'
 import Meals from './components/Meals.js'
-import { CartProv } from './store/CartContext.js';
+import CartContext from './store/CartContext.js';
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD_ITEM") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    )
+
+    let updatedItems
+
+    if (existingCartItemIndex !== -1) {
+      updatedItems = [...state.items]
+      updatedItems[existingCartItemIndex] = {
+        ...updatedItems[existingCartItemIndex],
+        quantity: updatedItems[existingCartItemIndex].quantity + 1,
+      }
+
+    } else {
+      updatedItems = [...state.items, { ...action.item, quantity: 1 }]
+    }
+
+    return { items: updatedItems }
+  }
+
+  return state
+}
 
 const App = () => {
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, { items: [] })
+ 
+   const addItemToCartHandler = (item) => {
+     dispatchCartAction({ type: "ADD_ITEM", item })
+   };
  return (
     <>
-    <CartProv>
+     <CartContext.Provider value={{ items: cartState.items, addItem: addItemToCartHandler }}>
 
       <Header />
       <main>
         <Meals />
       </main>
-      </CartProv>
+      </CartContext.Provider>
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,26 @@
-import logo from '../assets/logo.jpg'
-import Button from './UI/Button.js'
-import { useContext, useEffect } from 'react'
-import CartContext from '../store/CartContext.js'
+import { useContext } from "react"
+import logo from "../assets/logo.jpg"
+import Button from "./UI/Button"
+import CartContext from "../store/CartContext"
+
 const Header = () => {
-    const cartCtx = useContext(CartContext)
- 
-    useEffect(() => {
-      const itemQuantities = cartCtx.items.reduce((acc, item) => {
-        acc[item.id] = (acc[item.id] || 0) + 1
-        return acc
-      }, {})
-  
-      const cartItemsWithQuantity = Object.values(
-        cartCtx.items.reduce((acc, item) => {
-          if (!acc[item.id]) {
-            acc[item.id] = { ...item, quantity: itemQuantities[item.id] }
-          }
-          return acc
-        }, {})
-      );
-  
-      console.log('Cart items:', cartItemsWithQuantity)
-    }, [cartCtx.items])
-  
-    const totalItems = cartCtx.items.length
-  
-    return (
-        <header id="main-header">
-            <div id="title">
-                <img src={logo}/>
-                <h1>React Food Order App</h1>
-            </div>
-            <nav>
-            <Button textOnly>Cart (0)</Button>
-            </nav>
-        </header>
-    )
+     const cartCtx = useContext(CartContext)
+
+  const totalItems = cartCtx.items.reduce((total, item) => total + item.quantity, 0)
+
+  console.log("Cart items:", cartCtx.items)
+
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logo} alt="Food Order Logo" />
+        <h1>React Food Order App</h1>
+      </div>
+      <nav>
+        <Button textOnly>Cart ({totalItems})</Button>
+      </nav>
+    </header>
+  )
 }
 
 export default Header;
