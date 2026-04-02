b6174c1a6449a455f2df1f34b8fd40fa85763763
polluste888 <bert-robert.polluste@voco.ee>
2026-04-02 12:55:47 +0300

    ül3

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 
 fetch("/meals")
  .then(res => res.json())
@@ -8,13 +9,27 @@ fetch("/meals")
 
 
 const Meals = () => {
-    return (
-        <ul id="meals">
-            { 
-                // list of meals
+   const [meals, setMeals] = useState([])
+   useEffect(() => {
+        const fetchMeals = async () => {
+            try {
+                // Tuleb paring backendist
+                const response = await fetch("http://localhost:3001/meals") 
+                if (!response.ok) {
+                    throw new Error("Failed to fetch meals.")
+                }
+                const data = await response.json() 
+                console.log(data)
+                setMeals(data)
+            } catch (error) {
+                console.error("Error fetching meals:", error)
             }
-        </ul>
-    )
+                 }
+
+        fetchMeals()
+    }, [])
+
+    return null
 }
 
 export default Meals;
